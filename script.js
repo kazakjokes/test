23f7e9da583bb742d59e5668fa29ecf6c8b5df43
Dimon <kazakjokes@gmail.com>
2019-01-09 02:31:47 +0200

    FIsrt commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+// let arr=[1,2,3];
+
+// console.log(arr);
+
+// let answer=confirm("OK?");
+
+// console.log(answer);
+
+
+// let answer=prompt("Are you Ok?", "Yes");
+
+// console.log(typeof(null));
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(21%7);
+console.log(--decr);
+
